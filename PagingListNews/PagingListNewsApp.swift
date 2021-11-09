cfc2c05e44d6b8aa3d73e12669649e0d07c0a907
Пресняков Кирилл Максимович <KMPresnyakov@sberbank.ru>
2021-11-09 21:42:37 +0300

    Initial Commit

---
+++ PagingListNews/PagingListNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagingListNewsApp.swift
+//  PagingListNews
+//
+//  Created by 18898505 on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PagingListNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
